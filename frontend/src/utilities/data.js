894233dc5f4a57b520d8ee6308e544265eb3ac59
Kibeh <kibetimmanuel0@gmail.com>
2025-01-21 14:34:25 +0300

    create map using leaflet

---
+++ frontend/src/utilities/data.js
@@ -0,0 +1,18 @@
+export const facilities = [
+  {
+    id: 1,
+    name: 'Facility A',
+    latitude: -1.2921,
+    longitude: 36.8219,
+    cold_chain_capacity: 500,
+    available_space: 200,
+  },
+  {
+    id: 2,
+    name: 'Facility B',
+    latitude: -0.5143,
+    longitude: 37.4527,
+    cold_chain_capacity: 300,
+    available_space: 100,
+  },
+]
